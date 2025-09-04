7fa2193c12ebe38ae8573da8c1ffc15a1d1ce291
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-09-04 17:17:01 +0200

    fix controller and routes for entity correlation

---
+++ router/productsRouter.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const productsRouter = express.Router()
+
+const productsController = require("../controller/productsController")
+
+// ATTENZIONE LE ROTTE SONO PROVVISORIE
+
+productsRouter.get("/", productsController.index)
+
+productsRouter.get("/:id", productsController.show)
+
+module.exports = productsRouter
