bdfe8a4a2b259fed9538f447b4828063df3af7d4
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-09-04 15:40:40 +0200

    Update package dependencies and remove unused API endpoint

--- server.js
+++ server.js
@@ -13,16 +13,3 @@ app.listen(PORT, () => {
 
 app.use(cors())
 
-//INDEX
-app.get('/api/products', (req, res) => {
-    //SAVING THE QUERY STRING INTO A VARIABLE
-    const sql = 'SELECT * FROM boolcore_db.products'
-    connection.query(sql, (err, result) => {
-
-        //Manage 500 error
-        if (err) return res.status(500).json({ error: 'true', message: err.message })
-
-        res.json(result)
-    })
-
-})
